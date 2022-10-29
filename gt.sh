35bb0a298021b754c36a427cceaa0e7a11b4981d
rith67 <rithvik.ganeshhs@gmail.com>
2022-10-29 22:21:25 +0530

    Recorrecting last commit

--- gt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash                                                                     
-
-# Run this script with an argument which will be the commit message             
-git pull
-git add .
-git commit -m "$1"
-git push
--- store/main.go
+++ store/main.go
@@ -11,28 +11,28 @@ func main() {
 	store.Clean()
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
-	r.POST("/store/food/reserve", func(c *gin.Context){
+	r.POST("/store/food/reserve", func(c *gin.Context) {
 		var req store.ReserveFoodRequest
-		if err := c.ShouldBindJSON(&req); err != nil{
+		if err := c.ShouldBindJSON(&req); err != nil {
 			c.AbortWithStatus(400)
 		}
 
 		_, err := store.ReserveFood(req.FoodID)
-		if err != nil{
+		if err != nil {
 			c.AbortWithError(500, err)
 		}
 
 		c.JSON(200, store.ReserveFoodResponse{Reserved: true})
 	})
 
-	r.POST("/store/food/book", func(c *gin.Context){
+	r.POST("/store/food/book", func(c *gin.Context) {
 		var req store.BookFoodRequest
-		if err := c.ShouldBindJSON(&req); err != nil{
+		if err := c.ShouldBindJSON(&req); err != nil {
 			c.AbortWithStatus(400)
 		}
 
 		packet, err := store.BookFood(req.OrderID, req.FoodID)
-		if err != nil{
+		if err != nil {
 			log.Fatal(err)
 			c.AbortWithError(500, err)
 		}
